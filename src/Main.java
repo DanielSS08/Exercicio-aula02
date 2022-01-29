a3a6fdd614b7b480104e032220343cd667ff0a4f
DanielSS08 <dsilva.soares08@gmail.com>
2022-01-29 18:57:31 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício quadrilatero:");
+
+        Quadrilatero.area(4);
+        Quadrilatero.area(8,5);
+        Quadrilatero.area(6,4,3);
+
+    }
+}
